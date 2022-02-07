6be65f695fabfd47327e5a79a4c22b4c42a59112
Hunter Bowles <bowleshg@dukes.jmu.edu>
2022-02-07 16:14:48 -0500

    Expanded Character Class to include actions and status changes. May implement to superclass, as Enemies have similar abilities.

--- characters_enemies/character.js
+++ characters_enemies/character.js
@@ -20,13 +20,56 @@ class Character { // Object class for player characters
 
    hurt(points) {
       this.hp -= points;
+      console.log("You took " + points + " points of damage!");
       if (this.hp <= 0) {
          this.conditon = death;
+         console.log("You have died! GAME OVER");
       }
    }
 
    heal(points) {
       this.hp += points;
+      console.log("You healed for " + points + "points!");
+   }
+
+   condition_effect(condition) {
+      switch(conditon) {
+         case "poison":
+            this.hurt(3);
+         case "prone":
+            this.armor /= 2;
+            this.damage /= 2;
+      }
+   }
+
+   action(act) {
+      switch(act) {
+         case "Stand Up":
+            if (this.condition == "prone") {
+               this.condition = null;
+               this.armor *= 2;
+               this.damage *= 2;
+            }
+         case "Attack":
+            let atk = Math.random();
+            if (atk == 1.0) {
+               console.log("Critical Hit!");
+               enemy.hurt(this.damage * 2);
+            } else if (atk * 20 >= enemy.armor) {
+               enemy.hurt(this.damage);
+            } else {
+               console.log("Attack Missed!");
+            }
+         case "Use Potion":
+            this.heal(5);
+         case "Use Antidote":
+            if (this.condition == "poison") {
+               this.condition = null;
+               console.log("Cured Poison!");
+            }
+         case "Flee":
+            // END the game.
+      }
    }
 
    
